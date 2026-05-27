23e837797770a24b63a5ed95ad3c4f96ccea614c
JacZerin24 <jaczerin24@gmail.com>
2026-05-27 13:58:17 -0500

    Create build_month.py
    Add build month placeholder script

---
+++ scripts/build_month.py
@@ -0,0 +1,8 @@
+def main():
+    print("build_month.py placeholder")
+    print("Future usage:")
+    print("python .\\scripts\\build_month.py --year 2024 --month 5 --wfo LIX")
+
+
+if __name__ == "__main__":
+    main()
